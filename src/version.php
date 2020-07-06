0a90d162f275e3e34ace43cdb317aa1b87a06c13
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-07-06 09:46:19 -0400

    r9

--- src/version.php
+++ src/version.php
@@ -21,13 +21,13 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020052700;       // The current module version (Date: YYYYMMDDXX)
-$plugin->release = 'R8-V1.8';
+$plugin->version   = 2020062900;       // The current module version (Date: YYYYMMDDXX)
+$plugin->release = 'R9-V1.9';
 $plugin->requires  = 2018050800;    // Requires this Moodle version
 $plugin->component = 'local_recitdashboard';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
 $plugin->dependencies = [                                                                                                           
-    'local_recitcommon' => 2020052700
+    'local_recitcommon' => 2020062900
 ];
 //$plugin->release = '1.0'; 
 $plugin->maturity = MATURITY_ALPHA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
