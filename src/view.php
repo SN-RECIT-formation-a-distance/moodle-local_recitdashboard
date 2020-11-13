a7fe65d05d775bd805eaf64ec3d929705f8a25c6
Gustavo Bazzo <gcbazzo@gmail.com>
2020-11-13 15:05:35 -0500

    Update view.php

--- src/view.php
+++ src/view.php
@@ -55,7 +55,7 @@ require_login();
 // Globals.
 $PAGE->set_url("/local/recitdashboard/view.php");
 $PAGE->requires->css(new moodle_url($CFG->wwwroot . '/local/recitdashboard/react_app_build/index.css'), true);
-$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/local/recitdashboard/react_app_build/index.js?v='.rand(0,100)), true);
+$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/local/recitdashboard/react_app_build/index.js'), true);
 
 // Set page context.
 $PAGE->set_context(context_system::instance());
