d83e98a1981fb1fbf120bd706f82bdee326e47a4
Émile Jacques <emilevirus@gmail.com>
2022-07-12 14:55:49 -0400

    Add missing copyright #4

--- src/classes/recitcommon/PersistCtrl.php
+++ src/classes/recitcommon/PersistCtrl.php
@@ -2,7 +2,7 @@
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License published by
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
@@ -12,12 +12,14 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
- 
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
 /**
+ * RÉCIT Dashboard
  *
- * @copyright  2019 RÉCIT
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   local_recitdashboard
+ * @copyright 2019 RÉCIT 
+ * @license   {@link http://www.gnu.org/licenses/gpl-3.0.html} GNU GPL v3 or later
  */
 
 namespace recitdashboard;
