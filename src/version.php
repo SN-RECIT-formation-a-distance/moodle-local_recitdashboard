acba9f229e920d7f0dd8990ddb5c3389e3d20dce
Gustavo Bazzo <gcbazzo@gmail.com>
2025-02-25 11:31:02 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024072500;       // The current module version (Date: YYYYMMDDXX)
-$plugin->release = 'v1.18.11-stable';
-$plugin->supported = [39, 405];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
-$plugin->requires  = 2020061500.00; // Moodle 3.9.0
+$plugin->version   = 2025013000;       // The current module version (Date: YYYYMMDDXX)
+$plugin->release = 'v2.0.0-stable';
+$plugin->supported = [405, 405];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
+$plugin->requires  = 2024071200.00; // Moodle 4.5.0
 $plugin->component = 'local_recitdashboard';        // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
